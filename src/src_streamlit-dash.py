7d8460a47f18912a605691f61120659290e7b645
jacKlinc <jackharding321@gmail.com>
2020-09-03 09:39:19 +0100

    Added slide bar to control the limits of the scatterplot. Slide bars added in the sidebar.

--- src/src_streamlit-dash.py
+++ src/src_streamlit-dash.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-@st.cache()
+@st.cache
 def get_movies():
     movie_ratings = pd.read_csv('./data/movies_metadata.csv', usecols=['revenue', 'runtime'])
     # Update data types
@@ -20,9 +20,24 @@ def get_movies():
 
 movies = get_movies()
 
-# Show top movies
-st.dataframe(movies.head())
+st.title("Movie Dashboard")
 
+# Runtime limits using sliders in sidebar
+st.sidebar.markdown('### Runtime')  
+runtime1 = st.sidebar.slider('Runtime lower limit', 0, movies.runtime.max(), 0, step=5)
+runtime2 = st.sidebar.slider('Runtime upper limit', 0, movies.runtime.max(), movies.runtime.max(), step=5)
+
+# Revenue limits using sliders in sidebar
+st.sidebar.markdown('### Revenue')
+revenue1 = st.sidebar.slider('Revenue lower limit', 0, movies.revenue.max(), 0, step=10000)
+revenue2 = st.sidebar.slider('Revenue upper limit', 0, movies.revenue.max(), movies.revenue.max(), step=10000)
+
+filtered = movies[
+    (movies['runtime'] >= runtime1) & (movies['runtime'] < runtime2) &
+    (movies['revenue'] >= revenue1) & (movies['revenue'] < revenue2)
+]
+
+'Control graph limits in the sidebar'
 # Graph runtime vs revenue
-sns.scatterplot(x='runtime', y='revenue', data=movies)
+sns.scatterplot(x='runtime', y='revenue', data=filtered)
 st.pyplot()
